d13c9c023b2934d594f888cbed8cd37cf4024570
Karel <karel@codeguy.cz>
2021-08-10 17:55:46 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Invoices from './components/Invoices';
+import './style/App.scss';
+
+function App() {
+  return (
+    <>
+      <Invoices />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Invoices.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../style/Invoices.scss';
+
+function Invoices() {
+    return (
+        <>
+            <div className="card">
+                <p>
+                    Hello World
+                </p>
+            </div>
+        </>
+    );
+}
+
+export default Invoices;
